18ffb87c3ea66a4d94eeae1500b9b1438019d2c2
ludha <1214002@std.ulbi.ac.id>
2023-11-17 00:15:21 +0700

    push

---
+++ type.go
@@ -0,0 +1,12 @@
+package webhook
+
+type Reply struct {
+	Message string `bson:"messsage"`
+}
+
+var stringreply = []string{
+	"Jangan SPAAM woiii berisik lagi turuuuuu",
+	"Kamu hensommm bgt tau",
+	"genchananaaa genchanaaaayoooo",
+	"Jangan galak dong",
+}
